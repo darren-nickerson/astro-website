4f723f24bd73a810ab9b08d1a3450fd2a0efea19
darren nickerson <darren.nickerson@milcroft.com>
2023-08-07 17:45:47 +0100

    completed tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
-   viewTransitions: true
-  }
+    viewTransitions: true
+  },
+  integrations: [preact()]
 });
